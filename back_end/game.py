30696138e9e0d529d753a4379b1249f7730a20d0
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-01-20 23:52:09 +0800

    game

---
+++ back_end/game.py
@@ -0,0 +1,15 @@
+class Game():
+    # game state
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.turn = 1
+        self.winner = None
+    # def process(str ...)  // client input
+        
+
+
+    # def get_state(self): // client output
+        
+
+    
