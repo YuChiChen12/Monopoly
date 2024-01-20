7bea9bec727c8bcb281bfb8ff337f2383f6b23f5
YuChiChen113 <chi.mg10@nycu.edu.tw>
2024-01-21 02:58:47 +0800

    game

--- back_end/game.py
+++ back_end/game.py
@@ -1,15 +1,80 @@
+from flask import jsonify
+
+# Input json：
+    # "squad_num"
+    # "stop_num"
+    # "game_gain"
+    # "extra_gain"
+    # "chance"
+    # "add_asset"
+
+# Output json：
+    # "cash_per_squad"
+    # "bankrupt_time_per_squad"
+    # "asset_per_stop"
+    # "toll_per_stop"
+
 class Game():
-    # game state
+    # 初始值
     def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-        self.turn = 1
-        self.winner = None
+        self.cash_per_squad = []   # cash_per_squad：每隊現金 (1D array)
+        for _ in range(8):
+            self.cash_per_squad.append(5000)
+        self.bankrupt_time_per_squad = []   # bankrupt_time_per_squad：每隊破產次數 (1D array)
+        for _ in range(8):
+            self.bankrupt_time_per_squad.append(0)
+        self.asset_per_stop = []   # asset_per_stop：每隊在各關的房地產數量 (2D array)
+        for _ in range(15):
+            for i in range(8):
+                self.asset_per_stop.append(0)
+        self.toll_per_stop = []   # toll_per_stop：每隊經過各關的過路費 (2D array)
+        for _ in range(15):
+            for i in range(8):
+                self.toll_per_stop.append(0)
+
+    # 遊戲獎金 & 機會/命運 & 過路費
+    def gain_and_toll(self, data):
+        # 破產(過路費>手上現金)
+        if self.toll_per_stop[data['stop_num']][data['squad_num']] > self.cash_per_squad[data['squad_num']]:
+            self.cash_per_squad[data['squad_num']] = 5000
+            self.bankrupt_time_per_squad[['squad_num']] += 1
+        # 置產前剩餘金額(收過路費後未破產)
+        else:
+            self.cash_per_squad[data['squad_num']] += data['game_gain'] + data['extra_gain'] + data['chance'] - self.toll_per_stop[data['stop_num']][data['squad_num']]
+
+    # 置產
+    def real_estate(self, data):
+        self.asset_per_stop[data['stop_num']][data['squad_num']] += data['add_asset']
+        for i in range(8):
+            if (i+1) != data['squad_num']:
+                self.toll_per_stop[data['stop_num']][i+1] += 1000 * data['add_asset']
+        if data['add_asset'] == 1:
+            self.cash_per_squad[data['squad_num']] -= 500
+        elif data['add_asset'] == 2:
+            self.cash_per_squad[data['squad_num']] -= 1300
+        elif data['add_asset'] == 3:
+            self.cash_per_squad[data['squad_num']] -= 2500
+    
     # def process(str ...)  // client input
-        
+    def data_processing(self, json_string):
+        data = json.parse(json_string)
+
+        for key, value in data.items():
+            data[key] = int(value)
+        if data['id'] == 1:
+            self.gain_and_toll(data)
+        elif data['id'] == 2:
+            self.real_estate(data)
+
+
+
 
 
     # def get_state(self): // client output
-        
-
-    
+    def get_state(self):
+        return jsonify({
+            "cash_per_squad" : self.cash_per_squad,
+            "bankrupt_time_per_squad" : self.bankrupt_time_per_squad,
+            "asset_per_stop" : self.asset_per_stop,
+            "toll_per_stop" : self.toll_per_stop
+        })
--- front_end/src/components/Score.js
+++ front_end/src/components/Score.js
@@ -3,14 +3,12 @@ import React, { useState } from 'react';
 function Score() {
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label>第幾小隊：</label><input id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-        <br />
-        <input type="submit" value="送出表單" />
-      </form>
-      <div>
-        {cashPerSquad}
-      </div>
+      <form>
+                <label>姓名：</label>
+                <input id="name" name="name" />
+                <br/>
+                    <input type="submit" value="送出表單"  />
+            </form>
     </>
   );
 }
