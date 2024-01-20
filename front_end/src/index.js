ba47a6f949e7af280b4dea12bb68f5bdbe9adac2
YuChiChen113 <chi.mg10@nycu.edu.tw>
2024-01-21 02:55:29 +0800

    game

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -2,12 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
-// 變數宣告
-const squads = 8;
-const stops = 15;
-window.cashPerSquad = Array.from({ length: squads }).fill(0);
-window.assetPerStop = Array.from({ length: stops }).fill(0);
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
