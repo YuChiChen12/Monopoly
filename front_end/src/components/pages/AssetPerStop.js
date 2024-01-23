1ff244ad85f401f37e696bcc2bdd636886c6a551
YuChiChen113 <chi.mg10@nycu.edu.tw>
2024-01-24 01:27:45 +0800

    soso

--- front_end/src/components/pages/AssetPerStop.js
+++ front_end/src/components/pages/AssetPerStop.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
+import '../AssetPerStop.css';
 import Get from '../Api.js';
 
 function AssetPerStop() {
@@ -30,6 +31,48 @@ function AssetPerStop() {
 
   return (
     <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+          <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+          </tr>
+          <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+          </tr>
+          <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+          </tr>
+        </tbody>
+      </table>
+
       <div>AssetPerStop</div>
       {/* Display the data */}
       {data && (
